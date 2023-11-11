be484a8edf6be81a61c5ee6cd3bdaf7425e74407
QuocDuy03 <quocduyy.03@gmail.com>
2023-11-11 20:47:31 +0700

    change directory

--- config/database.js
+++ config/database.js
@@ -1,17 +1,24 @@
 const mysql = require('mysql');
 
-async function connect() {
-    try {
-        await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: null,
-            database: 'selling_clothes',
-        })
-        console.log('connect successfully');
-    } catch (error) {
-        console.log('connect failure');
-    }
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: null,
+    database: 'selling_clothes',
+});
+
+function connect() {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log('Connected to MySQL database');
+                resolve(connection);
+            }
+        });
+    });
 }
 
-module.exports = { connect }
+module.exports = { connect };
