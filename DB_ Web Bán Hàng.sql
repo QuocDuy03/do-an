ca3e4fcc9dac6245ebb560314920219a862e73d2
quangnltUIT <21522512@gm.uit.edu.vn>
2023-11-30 13:38:08 +0700

    Them table Product_Type

--- DB_ Web Bán Hàng.sql
+++ DB_ Web Bán Hàng.sql
@@ -33,7 +33,13 @@ CREATE TABLE `Products` (
   `updated_at` datetime,
   `deleted` int,
   `quantity` int DEFAULT 0,
+  `type` varchar(10),
   CONSTRAINT `fk_products_category_id` FOREIGN KEY (`category_id`) REFERENCES `Categories` (`id`)
+  CONSTRAINT `fk_products_type` FOREIGN KEY (`type`) REFERENCES `ProductTypes` (`name`)
+);
+
+CREATE TABLE `ProductTypes` (
+  `name` varchar(10) PRIMARY KEY
 );
 
 CREATE TABLE `ProductSizes` (
@@ -141,18 +147,6 @@ INSERT INTO Roles (id, name) VALUES
 (2, 'User');
 
 
--- Thêm dữ liệu mẫu cho bảng Users
-INSERT INTO Users (id, fullname, email, phone_number, address, password, role_id, created_at, updated_at)
-VALUES
-(1, 'Đặng Quốc Duy', 'G5_tech1@gmail.com', '405893', 'KTX B, DHQG HCM', 'Dalatech@1', 2, '2023-01-08', '2023-01-12'),
-(2, 'Nguyễn Văn Anh Huy', 'G5_tech2@gmail.com', '338542', 'Tân Lâp, Dĩ An, Bình Dương', 'Dalatech@2', 2, '2023-04-08', '2023-05-03'),
-(3, 'Lê Quốc Kiệt', 'G5_tech3@gmail.com', '257598', 'Man Thiện, Thủ Đức', 'Dalatech@3', 2, '2023-03-03', '2023-04-04'),
-(4, 'Tưởng Quang Minh', 'G5_tech4@gmail.com', '985421', 'KTX A, DHQG HCM', 'Dalatech@4', 2, '2023-01-03', '2023-03-04'),
-(5, 'Nguyễn Thành Đạt', 'G5_tech5@gmail.com', '128975', '29 Lê Văn Việt, Hiệp Phú, Thủ Đức', 'Dalatech@5', 2, '2022-01-04', '2023-02-04'),
-(6, 'Lê Quang Hoàng', 'G5_tech6@gmail.com', '654192', 'KTX B, DHQG HCM', 'Dalatech@6', 2, '2023-03-05', '2023-04-04'),
-(7, 'Phan Anh Tuấn', 'G5_tech7@gmail.com', '908124', 'Tân Lâp, Dĩ An, Bình Dương', 'Dalatech@7', 2, '2023-02-12', '2023-03-03'),
-(8, 'Đào Duy Tuấn', 'G5_tech8@gmail.com', '276589', 'Tân Lâp, Dĩ An, Bình Dương', 'Dalatech@8', 2, '2023-02-02', '2023-03-04');
-
 -- Thêm dữ liệu mẫu cho bảng Categories
 INSERT INTO Categories (id, name)
 VALUES
@@ -161,44 +155,27 @@ VALUES
 (3, 'Trẻ em');
 
 
--- Thêm dữ liệu mẫu cho bảng Products
-INSERT INTO Products (id, category_id, title, price, discount, thumbnail, description, created_at, updated_at, deleted)
+-- Thêm dữ liệu mẫu cho bảng Products với trường type
+INSERT INTO Products (id, category_id, type, title, price, discount, thumbnail, description, created_at, updated_at, deleted)
 VALUES
-(1, 1, 'AIRism Áo Polo Cổ Thường', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/448947/item/goods_09_448947.jpg?width=320', 'Mô tả về sản phẩm 1', '2023-01-08', '2023-01-12', 0),
-(2, 1, 'AIRism Áo Polo Gài Nút', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/441604/item/goods_00_441604.jpg?width=750', 'Mô tả về sản phẩm 2', '2023-01-08', '2023-01-12', 0),
-(3, 1, 'Áo Polo Dry-Ex Ngắn Tay', 391000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/433041/item/goods_69_433041.jpg?width=750', 'Mô tả về sản phẩm 3', '2023-01-08', '2023-01-12', 0),
-(4, 1, 'DRY-EX Áo Polo Ngắn Tay', 244000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/464897/item/goods_63_464897.jpg?width=750', 'Mô tả về sản phẩm 4', '2023-01-08', '2023-01-12', 0),
-(5, 1, 'Áo Sơ Mi Vải Jersey Kẻ Sọc Dài Tay', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/456227/item/goods_65_456227.jpg?width=750', 'Mô tả về sản phẩm 5', '2023-01-08', '2023-01-12', 0),
-(6, 1, 'Áo Sơ Mi Co Giãn Kẻ Caro', 790000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/456250/item/goods_68_456250.jpg?width=750', 'Mô tả về sản phẩm 6', '2023-01-08', '2023-01-12', 0),
-(7, 1, 'EZY Quần Jean Siêu Co Giãn', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/439178/item/goods_68_439178.jpg?width=750', 'Mô tả về sản phẩm 7', '2023-01-08', '2023-01-12', 0),
-(8, 1, 'Quần Jean Siêu Co Giãn', 980000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/455472/item/goods_09_455472.jpg?width=750', 'Mô tả về sản phẩm 8', '2023-01-08', '2023-01-12', 0),
-(9, 1, 'Quần Jeans Dáng Slim Fit Co Giãn', 980000, 0, 'https://image.uniqlo.com/UQ/ST3/vn/imagesgoods/447649/item/vngoods_09_447649.jpg?width=750', 'Mô tả về sản phẩm 9', '2023-01-08', '2023-01-12', 0),
-(10, 1, 'Quần Jeans Slim Fit', 980000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/452524/item/goods_63_452524.jpg?width=750', 'Mô tả về sản phẩm 10', '2023-01-08', '2023-01-12', 0),
-(11, 1, 'Quần Smart Pants', 588000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/451818/item/goods_07_451818.jpg?width=750', 'Mô tả về sản phẩm 11', '2023-01-08', '2023-01-12', 0),
-(12, 1, 'AirSense Quần Dài', 980000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/462618/item/goods_09_462618.jpg?width=750', 'Mô tả về sản phẩm 12', '2023-01-08', '2023-01-12', 0),
-(13, 2, 'Áo Thun Cổ Tròn', 293000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/462666/item/goods_31_462666.jpg?width=750', 'Mô tả về sản phẩm 13', '2023-01-08', '2023-01-12', 0),
-(14, 2, 'HEATTECH Áo Nỉ', 980000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/449878/item/goods_12_449878.jpg?width=750', 'Mô tả về sản phẩm 14', '2023-01-08', '2023-01-12', 0),
-(15, 2, 'Áo Nỉ Có Mũ Dài', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/419503/item/goods_09_419503.jpg?width=750', 'Mô tả về sản phẩm 15', '2023-01-08', '2023-01-12', 0),
-(16, 2, 'Áo Len Cổ Tròn', 588000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/450509001/item/goods_11_450509001.jpg?width=750', 'Mô tả về sản phẩm 16', '2023-01-08', '2023-01-12', 0),
-(17, 2, 'Áo Kiểu Vải Rayon', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/446846/item/goods_69_446846.jpg?width=750', 'Mô tả về sản phẩm 17', '2023-01-08', '2023-01-12', 0),
-(18, 2, 'PEACE FOR ALL (Hana Tajima)', 391000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/457857/item/goods_10_457857.jpg?width=750', 'Mô tả về sản phẩm 18', '2023-01-08', '2023-01-12', 0),
-(19, 2, 'Quần Dài Vải Tweed Xếp Ly Ống Rộng', 980000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/462077001/sub/goods_462077001_sub14.jpg?width=750', 'Mô tả về sản phẩm 19', '2023-01-08', '2023-01-12', 0),
-(20, 2, 'Quần Dài Vải Nhung Xếp Ly Ống Rộng', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/460541/sub/goods_460541_sub14.jpg?width=750', 'Mô tả về sản phẩm 20', '2023-01-08', '2023-01-12', 0),
-(21, 2, 'Quần Xếp Ly Ống Rộng', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/460311/sub/goods_460311_sub14.jpg?width=750', 'Mô tả về sản phẩm 21', '2023-01-08', '2023-01-12', 0),
-(22, 2, 'Quần Ống Suông Dáng Rộng Túi Hộp (Quần Cargo)', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/461643/sub/goods_461643_sub14.jpg?width=750', 'Mô tả về sản phẩm 22', '2023-01-08', '2023-01-12', 0),
-(23, 2, 'Quần Dài Vải Brushed Jersey Dáng Rộng', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/463706/sub/goods_463706_sub14.jpg?width=750', 'Mô tả về sản phẩm 23', '2023-01-08', '2023-01-12', 0),
-(24, 2, 'Quần Easy Pants Dáng Rộng Ống Suông', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/466000/sub/goods_466000_sub14.jpg?width=750', 'Mô tả về sản phẩm 24', '2023-01-08', '2023-01-12', 0),
-(25, 3, 'Áo Khoác Giả Lông Cừu Kéo Khóa Dài Tay', 391000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/464673/sub/goods_464673_sub14.jpg?width=750', 'Mô tả về sản phẩm 25', '2023-01-08', '2023-01-12', 0),
-(26, 3, 'Áo Khoác Giả Lông Cừu Kéo Khóa Dài Tay', 391000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/463190/sub/goods_463190_sub14.jpg?width=750', 'Mô tả về sản phẩm 26', '2023-01-08', '2023-01-12', 0),
-(27, 3, 'BÉ GÁI ÁO NỈ CÓ MŨ CHUI ĐẦU', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/467878/sub/goods_467878_sub14.jpg?width=750', 'Mô tả về sản phẩm 27', '2023-01-08', '2023-01-12', 0),
-(28, 3, 'Áo Hoodie Nỉ Dáng Lửng Dài Tay', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/467876/sub/goods_467876_sub14.jpg?width=750', 'Mô tả về sản phẩm 28', '2023-01-08', '2023-01-12', 0),
-(29, 3, 'UT Mickey Shines Áo Thun Ngắn Tay', 244000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/466424/sub/goods_466424_sub14.jpg?width=750', 'Mô tả về sản phẩm 29', '2023-01-08', '2023-01-12', 0),
-(30, 3, 'KAWS Áo Nỉ Dài Tay', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/467776/sub/goods_467776_sub14.jpg?width=750', 'Mô tả về sản phẩm 30', '2023-01-08', '2023-01-12', 0),
-(31, 3, 'Quần Jeans Baggy', 580000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/462976/sub/goods_462976_sub14.jpg?width=750', 'Mô tả về sản phẩm 31', '2023-01-08', '2023-01-12', 0),
-(32, 3, 'Quần Denim Siêu Co Giãn Ống Ôm Dần', 580000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/454364/sub/goods_454364_sub14.jpg?width=750', 'Mô tả về sản phẩm 32', '2023-01-08', '2023-01-12', 0),
-(33, 3, 'Quần Nỉ Dry Siêu Co Giãn', 391000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/463500/sub/goods_463500_sub14.jpg?width=750', 'Mô tả về sản phẩm 33', '2023-01-08', '2023-01-12', 0),
-(34, 3, 'Quần Váy Xếp Ly Kẻ Caro', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/460668003/sub/goods_460668003_sub14.jpg?width=750', 'Mô tả về sản phẩm 34', '2023-01-08', '2023-01-12', 0),
-(35, 3, 'Quần Jean Mềm Siêu Co Giãn', 391000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/454371/sub/goods_454371_sub14.jpg?width=750', 'Mô tả về sản phẩm 35', '2023-01-08', '2023-01-12', 0);
+(1, 1, 'Áo', 'AIRism Áo Polo Cổ Thường', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/448947/item/goods_09_448947.jpg?width=320', 'Mô tả về sản phẩm 1', '2023-01-08', '2023-01-12', 0),
+(2, 1, 'Áo', 'AIRism Áo Polo Gài Nút', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/441604/item/goods_00_441604.jpg?width=750', 'Mô tả về sản phẩm 2', '2023-01-08', '2023-01-12', 0),
+(3, 1, 'Áo', 'Áo Polo Dry-Ex Ngắn Tay', 391000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/433041/item/goods_69_433041.jpg?width=750', 'Mô tả về sản phẩm 3', '2023-01-08', '2023-01-12', 0),
+(4, 1, 'Áo', 'DRY-EX Áo Polo Ngắn Tay', 244000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/464897/item/goods_63_464897.jpg?width=750', 'Mô tả về sản phẩm 4', '2023-01-08', '2023-01-12', 0),
+(7, 1, 'Quần', 'EZY Quần Jean Siêu Co Giãn', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/439178/item/goods_68_439178.jpg?width=750', 'Mô tả về sản phẩm 7', '2023-01-08', '2023-01-12', 0),
+(8, 1, 'Quần', 'Quần Jean Siêu Co Giãn', 980000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/455472/item/goods_09_455472.jpg?width=750', 'Mô tả về sản phẩm 8', '2023-01-08', '2023-01-12', 0),
+(11, 1, 'Quần', 'Quần Smart Pants', 588000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/451818/item/goods_07_451818.jpg?width=750', 'Mô tả về sản phẩm 11', '2023-01-08', '2023-01-12', 0),
+(12, 1, 'Quần', 'AirSense Quần Dài', 980000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/462618/item/goods_09_462618.jpg?width=750', 'Mô tả về sản phẩm 12', '2023-01-08', '2023-01-12', 0),
+(13, 2, 'Áo', 'Áo Thun Cổ Tròn', 293000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/462666/item/goods_31_462666.jpg?width=750', 'Mô tả về sản phẩm 13', '2023-01-08', '2023-01-12', 0),
+(14, 2, 'Áo', 'HEATTECH Áo Nỉ', 980000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/449878/item/goods_12_449878.jpg?width=750', 'Mô tả về sản phẩm 14', '2023-01-08', '2023-01-12', 0),
+(15, 2, 'Áo', 'Áo Nỉ Có Mũ Dài', 784000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/419503/item/goods_09_419503.jpg?width=750', 'Mô tả về sản phẩm 15', '2023-01-08', '2023-01-12', 0),
+(16, 2, 'Áo', 'Áo Len Cổ Tròn', 588000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/450509001/item/goods_11_450509001.jpg?width=750', 'Mô tả về sản phẩm 16', '2023-01-08', '2023-01-12', 0),
+(31, 3, 'Quần', 'Quần Jeans Baggy', 580000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/462976/sub/goods_462976_sub14.jpg?width=750', 'Mô tả về sản phẩm 31', '2023-01-08', '2023-01-12', 0),
+(32, 3, 'Quần', 'Quần Denim Siêu Co Giãn Ống Ôm Dần', 580000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/454364/sub/goods_454364_sub14.jpg?width=750', 'Mô tả về sản phẩm 32', '2023-01-08', '2023-01-12', 0),
+(33, 3, 'Quần', 'Quần Nỉ Dry Siêu Co Giãn', 391000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/463500/sub/goods_463500_sub14.jpg?width=750', 'Mô tả về sản phẩm 33', '2023-01-08', '2023-01-12', 0),
+(34, 3, 'Quần', 'Quần Váy Xếp Ly Kẻ Caro', 489000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/460668003/sub/goods_460668003_sub14.jpg?width=750', 'Mô tả về sản phẩm 34', '2023-01-08', '2023-01-12', 0),
+(35, 3, 'Quần', 'Quần Jean Mềm Siêu Co Giãn', 391000, 0, 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/454371/sub/goods_454371_sub14.jpg?width=750', 'Mô tả về sản phẩm 35', '2023-01-08', '2023-01-12', 0);
+
 
 
 -- Thêm dữ liệu mẫu cho bảng Galeries
@@ -251,6 +228,10 @@ VALUES
 (20, 5, 'Nguyễn Thành Đạt', 'G5_tech5@gmail.com', 128975, '29 Lê Văn Việt, Hiệp Phú, Thủ Đức', 'Giao trong 72h');
 
 
+INSERT INTO `ProductTypes` (`name`) VALUES
+('Áo'),
+('Quần');
+
 -- Thêm dữ liệu mẫu cho bảng Order_Details
 INSERT INTO Order_Details (id, order_id, product_id, price, num, total_money)
 VALUES
@@ -265,3 +246,116 @@ VALUES
 (15, 15, 24, 784000, 2, 1568000),
 (18, 18, 18, 391000, 1, 391000),
 (20, 20, 13, 293000, 2, 586000);
+
+-- Chèn dữ liệu vào bảng ProductSizes
+INSERT INTO ProductSizes (product_id, size, quantity)
+VALUES
+(1, 'S', 10),
+(1, 'M', 17),
+(1, 'L', 18),
+(2, 'S', 3),
+(2, 'M', 2),
+(2, 'L', 15),
+(3, 'S', 14),
+(3, 'M', 19),
+(3, 'L', 10),
+(4, 'S', 5),
+(4, 'M', 13),
+(4, 'L', 15),
+(5, 'S', 12),
+(5, 'M', 13),
+(5, 'L', 7),
+(6, 'S', 14),
+(6, 'M', 6),
+(6, 'L', 6),
+(7, 'S', 16),
+(7, 'M', 9),
+(7, 'L', 10),
+(8, 'S', 4),
+(8, 'M', 10),
+(8, 'L', 13),
+(9, 'S', 10),
+(9, 'M', 18),
+(9, 'L', 19),
+(10, 'S', 11),
+(10, 'M', 15),
+(10, 'L', 2),
+(11, 'S', 12),
+(11, 'M', 18),
+(11, 'L', 16),
+(12, 'S', 6),
+(12, 'M', 15),
+(12, 'L', 14),
+(13, 'S', 14),
+(13, 'M', 9),
+(13, 'L', 12),
+(14, 'S', 12),
+(14, 'M', 17),
+(14, 'L', 6),
+(15, 'S', 1),
+(15, 'M', 10),
+(15, 'L', 15),
+(16, 'S', 12),
+(16, 'M', 13),
+(16, 'L', 12),
+(17, 'S', 3),
+(17, 'M', 2),
+(17, 'L', 7),
+(18, 'S', 9),
+(18, 'M', 3),
+(18, 'L', 1),
+(19, 'S', 1),
+(19, 'M', 17),
+(19, 'L', 2),
+(20, 'S', 18),
+(20, 'M', 6),
+(20, 'L', 1),
+(21, 'S', 10),
+(21, 'M', 14),
+(21, 'L', 6),
+(22, 'S', 4),
+(22, 'M', 11),
+(22, 'L', 17),
+(23, 'S', 3),
+(23, 'M', 15),
+(23, 'L', 3),
+(24, 'S', 3),
+(24, 'M', 16),
+(24, 'L', 2),
+(25, 'S', 12),
+(25, 'M', 1),
+(25, 'L', 17),
+(26, 'S', 12),
+(26, 'M', 9),
+(26, 'L', 3),
+(27, 'S', 18),
+(27, 'M', 17),
+(27, 'L', 2),
+(28, 'S', 17),
+(28, 'M', 17),
+(28, 'L', 11),
+(29, 'S', 9),
+(29, 'M', 14),
+(29, 'L', 18),
+(30, 'S', 1),
+(30, 'M', 6),
+(30, 'L', 3),
+(31, 'S', 15),
+(31, 'M', 14),
+(31, 'L', 19),
+(32, 'S', 9),
+(32, 'M', 5),
+(32, 'L', 3),
+(33, 'S', 9),
+(33, 'M', 18),
+(33, 'L', 18),
+(34, 'S', 2),
+(34, 'M', 3),
+(34, 'L', 11),
+(35, 'S', 19),
+(35, 'M', 13),
+(35, 'L', 1),
+(36, 'S', 12),
+(36, 'M', 15),
+(36, 'L', 2);
+
